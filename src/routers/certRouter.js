45c15dfbb13105052689eb331e4c9520c395f2f4
Jeymz <jeymzsimmons@live.com>
2025-06-07 09:25:24 -0400

    Log certificate errors and test

--- src/routers/certRouter.js
+++ src/routers/certRouter.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const controller = require('../controllers/certController');
 const config = require('../resources/config')();
+const logger = require('../utils/logger');
 
 const router = express.Router();
 
 module.exports = () => {
-  router.route('/new')
-    .post((req, res) => {
-      if (!req.body || typeof req.body !== 'object') {
-        return res.status(400).send({
-          error: 'Invalid request body',
-        });
-      }
+    router.route('/new')
+      .post((req, res) => {
+        if (!req.body || typeof req.body !== 'object') {
+          return res.status(400).send({
+            error: 'Invalid request body',
+          });
+        }
       const validator = config.getValidator();
       if (!validator.validateSchema('new', req.body)) {
         return res.status(400).send('Invalid request body');
@@ -21,9 +22,14 @@ module.exports = () => {
         altNames,
         passphrase,
       } = req.body;
-      const newCert = controller.newWebServerCertificate(hostname, passphrase, altNames);
-      return res.send(newCert);
-    });
+      try {
+        const newCert = controller.newWebServerCertificate(hostname, passphrase, altNames);
+        return res.send(newCert);
+      } catch (err) {
+        logger.error(`Error creating certificate: ${err.message}`);
+        return res.status(400).send({ error: 'Unable to process request' });
+      }
+      });
 
   router.route('/')
     .get((req, res) => {
