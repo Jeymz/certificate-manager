969d898c981031a2f1c1f6252b0a70f6041425de
Jeymz Simmons <james@robotti.io>
2025-06-10 19:21:12 -0400

    Modifications to prevent directory from being over-written by tests
    - Not a great way to handle this but only temporary.

--- tests/globalTeardown.js
+++ tests/globalTeardown.js
@@ -3,7 +3,11 @@ const path = require('path');
 
 module.exports = async() => {
   const dest = path.join(__dirname, '../config/defaults.json');
-  if (fs.existsSync(dest)) {
+  const testStatus = path.join(__dirname, '../config/testStatus.txt');
+  if (fs.existsSync(dest) && fs.existsSync(testStatus)) {
     fs.unlinkSync(dest);
+  }
+  if (fs.existsSync(testStatus)) {
+    fs.unlinkSync(testStatus);
   }
 };
--- tests/preSetup.js
+++ tests/preSetup.js
@@ -3,6 +3,10 @@ const path = require('path');
 
 const src = path.join(__dirname, '../config/defaults.example.json');
 const dest = path.join(__dirname, '../config/defaults.json');
+const testStatus = path.join(__dirname, '../config/testStatus.txt');
 if (!fs.existsSync(dest)) {
+  fs.writeFileSync(testStatus, "testing");
   fs.copyFileSync(src, dest);
+} else if (!fs.existsSync(testStatus)) {
+  process.exit(1);
 }
